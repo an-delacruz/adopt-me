0ff85293f63a4ccff624d93ad66b5464bf3d2478
antoniocruzrivera0216 <antoniocruzrivera0216@gmail.com>
2024-01-07 03:57:05 +0000

    Results, Pet, Details, SearchParams Components

--- src/Pet.jsx
+++ src/Pet.jsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 // const Pet = (props) => {
 //   return React.createElement("div", {}, [
 //     React.createElement("h1", {}, props.name),
@@ -6,13 +8,23 @@
 //   ]);
 // };
 
-const Pet = (props) => {
+const Pet = ({ name, animal, breed, images, location, id }) => {
+  let hero = "https://pets-images.devs-apis.com/pets/none.jpg";
+  if (images.length) {
+    hero = images[0];
+  }
   return (
-    <div>
-      <h1>{props.name}</h1>
-      <h2>{props.animal}</h2>
-      <h2>{props.breed}</h2>
-    </div>
+    <Link to={`/details/${id}`} className="pet">
+      <div className="image-container">
+        <img src={hero} alt={name} />
+      </div>
+      <div className="info">
+        <h1>{name}</h1>
+        <h2>
+          {animal} - {breed} - {location}
+        </h2>
+      </div>
+    </Link>
   );
 };
 
--- src/SearchParams.jsx
+++ src/SearchParams.jsx
@@ -1,9 +1,36 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import useBreedList from "./useBreedList";
+import Results from "./Results";
+const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("");
+  const [animal, setAnimal] = useState("");
+  const [breed, setBreed] = useState("");
+  const [pets, setPets] = useState([]);
+
+  const [breeds] = useBreedList(animal);
+
+  useEffect(() => {
+    requestPets();
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  async function requestPets() {
+    const res = await fetch(
+      `https://pets-v2.dev-apis.com/pets?animal=${animal}&location=${location}&breed=${breed}`
+    );
+    const data = await res.json();
+    setPets(data.pets);
+  }
+
   return (
     <div className="search-params">
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          requestPets();
+        }}
+      >
         <label htmlFor="location">
           Location
           <input
@@ -13,8 +40,41 @@ const SearchParams = () => {
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
+        <label htmlFor="animal">
+          Animal
+          <select
+            id="animal"
+            value={animal}
+            onChange={(e) => {
+              setAnimal(e.target.value);
+              setBreed("");
+            }}
+          >
+            <option></option>
+            {ANIMALS.map((animal) => (
+              <option key={animal}>{animal}</option>
+            ))}
+          </select>
+        </label>
+        <label htmlFor="breed">
+          Breed
+          <select
+            id="breed"
+            value={breed}
+            onChange={(e) => {
+              setBreed(e.target.value);
+            }}
+            disabled={breeds.length == 0}
+          >
+            <option></option>
+            {breeds.map((breed) => (
+              <option key={breed}>{breed}</option>
+            ))}
+          </select>
+        </label>
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
